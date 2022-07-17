0ad9c99cebe8b496d29aaf0d96a559aac792f17a
Hazal Senturk <77551492+hazalsenturk@users.noreply.github.com>
2022-07-17 14:57:44 -0700

    Add files via upload

---
+++ day-1-printing-start/main.py
@@ -0,0 +1,7 @@
+# Write your code below this line 👇
+print("Hello world!\nHello world!\nHello world!")
+print("Hello"+" Hazal")
+print("Hello"+" "+"Hazal")
+# input() will get user input in console
+# then print() will print the "Hello" and the user input 
+print("Hello "+ input("What is your name?"))
